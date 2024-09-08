dbb5b22acfd80411e3f77d7f7350730fa8f91edc
0pavel1 <pavel.zamorskiy@mail.ru>
2024-09-08 19:54:28 +0300

    optimize algorithm

--- Primes.java
+++ Primes.java
@@ -8,7 +8,7 @@ public class Primes{
     }
     public static boolean isPrime(int n) {
         boolean flag = true;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n/2; i++) {
             if (n%i == 0){
                 flag = false;
                 break;
